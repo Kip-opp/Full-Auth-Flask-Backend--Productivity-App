1877031cc37ea303e0397e0039d92fb9bcee67de
denis@kyp <kypvega@gmail.com>
2026-04-18 13:41:08 +0300

    configure app runner for production

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from app import create_app
 
 app = create_app()
 
+@app.get("/")
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)  # or 5000 if free
